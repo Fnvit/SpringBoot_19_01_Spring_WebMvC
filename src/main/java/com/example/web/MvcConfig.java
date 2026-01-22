6eb081e150960ba17e29bcb10cef64d6d97cf2d6
fnvit <fnvit@naver.com>
2026-01-22 19:32:40 +0900

    Init

---
+++ src/main/java/com/example/web/MvcConfig.java
@@ -0,0 +1,33 @@
+package com.example.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc // WebMvc 활성화
+// WebMvcConfigurer를 구현함으로서, 기본적인 Web 설정을 Override(재설정) 할 수 있음
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/web/TestController.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("TEST 실행됨!");
+        return "test";
+    }
+}
